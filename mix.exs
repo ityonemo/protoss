d531ccbf59312752aebdfe83de57921ea2a30394
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2025-01-20 17:17:29 -0600

    version up to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Protoss.MixProject do
   def project do
     [
       app: :protoss,
-      version: "0.2.1",
+      version: "1.0.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
